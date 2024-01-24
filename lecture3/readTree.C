76dc92292ebd8d6ee16eeff82b316e8792742ae6
aebid <aebid@tamu.edu>
2024-01-24 20:22:46 +0100

    Adding starting point for Lab0, introduction to TTrees

---
+++ lecture3/readTree.C
@@ -0,0 +1,90 @@
+//A script to read a File with a TTree and plot some branches
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1.h>
+#include <TCanvas.h>
+#include <TPaveText.h>
+#include <TCut.h>
+
+void readTree()
+{
+  //Load the file and get the tree
+  TFile *hfile = new TFile("new_file.root");
+  TTree *tree = (TTree*)hfile->Get("tree_name");
+
+  //TTrees need to have a reference variable to put the data into
+  float uniform_dist;
+  float gaus_dist;
+  tree->SetBranchAddress("uniform_distribution", &uniform_dist);
+  tree->SetBranchAddress("gaus_distribution", &gaus_dist);
+
+  //Create a canvas to put plots onto
+  TCanvas *c1 = new TCanvas("c1", "Canvas", 1200, 1000);
+  //You can use the same canvas to draw multiple plots!
+  //canvas->Divide(n) will divide the canvas into n number of sections
+  //canvas->Divide(n,m) is also alowed
+  c1->Divide(3);
+
+
+  //Create some histograms to fill with the branch in a loop over entries (Option 1)
+  TH1F *uniform_hist = new TH1F("UniformHist", "Uniform Hist", 100, 0, 10);
+  TH1F *gaus_hist = new TH1F("GaussianHist", "Gaussian Hist", 100, 0, 10);
+
+  int nEntries = tree->GetEntries();
+  for (int i=0; i<nEntries; i++){
+    tree->GetEntry(i);
+
+    uniform_hist->Fill(uniform_dist);
+    gaus_hist->Fill(gaus_dist);
+  }
+
+  //Change to the first subplot of the canvas
+  c1->cd(1);
+  //hist->Draw("option") has many options
+  //Can find all draw options here https://root.cern/doc/master/classTHistPainter.html
+  //Lets use "E" to draw error bars
+  uniform_hist->Draw("E");
+  //Set the Y axis limits to be nicer to see
+  float uniform_hist_max = uniform_hist->GetMaximum();
+  uniform_hist->GetYaxis()->SetRangeUser(0.0, 1.5*uniform_hist_max);
+
+  //Change to the second subplot
+  c1->cd(2);
+  gaus_hist->Draw();
+  float gaus_hist_max = gaus_hist->GetMaximum();
+  gaus_hist->GetYaxis()->SetRangeUser(0.0, 1.5*gaus_hist_max);
+  //Change the line color, fill color, and fill style
+  gaus_hist->SetLineColor(kRed);
+  gaus_hist->SetFillColor(kOrange);
+  gaus_hist->SetFillStyle(3005);
+
+  //Add text onto the plot, use option "NDC" to use x/y positions in percentages
+  TPaveText *pt1 = new TPaveText(0.1,0.7,0.5,0.9, "NDC");
+  pt1->AddText("Example Text!");
+  pt1->Draw();
+
+
+  //Change to third subplot
+  c1->cd(3);
+
+  //Create some histograms to fill with tree->Draw("name >> hist", cut, option) (Option 2)
+  TH1F *hist_test = new TH1F("hist_test", "Hists cut by Gaus", 100, 0, 10);
+  TH1F *hist_test2 = new TH1F("hist_test2", "this_name_doesnt_matter", 100, 0, 10);
+  //TTree entries are connected, so each entry has both distributions
+  //We can cut the uniform distribution on what value the gaus distribution has
+  TCut cut1 = "gaus_distribution < 5";
+  //If we use tree->Draw("name >> hist") we can push branches directly into a histogram without looping all events!
+  tree->Draw("uniform_distribution >> hist_test", cut1);
+  tree->Draw("gaus_distribution >> hist_test2", cut1, "same");
+  hist_test2->SetLineColor(kRed);
+  hist_test->GetYaxis()->SetRangeUser(0.0, max(1.5*hist_test->GetMaximum(), 1.5*hist_test2->GetMaximum()));
+
+  TPaveText *pt2 = new TPaveText(0.1, 0.7, 0.7, 0.9, "NDC");
+  pt2->AddText("Variables in Trees are connected");
+  pt2->AddText("Notice the uniform dist lost half its entries");
+  pt2->Draw();
+
+  c1->SaveAs("example_plot.pdf");
+
+}
---
+++ lecture3/saveTree.C
@@ -0,0 +1,35 @@
+//A script to create and save a TTree object
+
+#include <TRandom.h>
+#include <TTree.h>
+#include <TFile.h>
+
+void saveTree() {
+  //Create a new file, "RECREATE" will delete an existing file and create a new one
+  TFile *hfile = new TFile("new_file.root", "RECREATE", "Example");
+
+  //Create a tree object, "tree_name" is the computer name, and "tree title" is the human name
+  TTree *tree = new TTree("tree_name", "tree title");
+
+  //Will use TRandom to generate values again
+  TRandom random;
+
+  //TTrees need to use references for data, the variable must already exist for the branch to be filled
+  float uniform_dist;
+  float gaus_dist;
+
+  //Add branches to the Tree, first argument is the branch name, second argument is the value reference, and third argument is the branch name / datatype
+  tree->Branch("uniform_distribution", &uniform_dist, "uniform_distribution/F");
+  tree->Branch("gaus_distribution", &gaus_dist, "gaus_distribution/F");
+
+  for (int i = 0; i < 100000; i++){
+    uniform_dist = random.Uniform(0.0,10.0);
+    gaus_dist = random.Gaus(5.0, 2.0);
+    //After setting the reference variables to the values we want, we fill the Tree
+    //All branches are filled with whatever their reference is
+    //BE SURE TO CHECK ALL REFERENCES, otherwise you will fill incorrect values
+    tree->Fill();
+  }
+  //Write the file
+  hfile->Write();
+}
